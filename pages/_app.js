a74db25a58c4ad3a9a2c75960f61b79d543066a0
Karina Barros <barros_kari@hotmail.com>
2025-02-04 15:26:55 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import { SEO } from '@/lib/seo';
+import Script from 'next/script';
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <DefaultSeo {...SEO} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
