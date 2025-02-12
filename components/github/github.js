43194e3b38bddc808b12384af76a60b1109a8cdd
Karina Barros <barros_kari@hotmail.com>
2025-02-12 10:37:38 -0300

    Gráfico do github

--- components/github/github.js
+++ components/github/github.js
@@ -1,50 +1,92 @@
-import axios from "axios";
-import React, { useEffect, useState } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './github.css'
 import LottieAnimationGithub from "../lottie/github-lottie";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export default function Github() {
-    const [chartData, setChartData] = useState([]);
+export default function Github2() {
+    const [loading, setLoading] = useState(false);
+    const [total, setTotal] = useState(null);
+    const [dados, setDados] = useState([]);
+    const [meses, setMeses] = useState([]);
+
+    async function fetchData() {
+        setLoading(true);
+        try {
+            const response = await axios.get('/api/github');
+            console.log(response.data);
+            setTotal(response.data.github.totalContributions);
+            setDados(response.data.github.weeks);
+            setMeses(response.data.meses);
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('/api/github');
-
-                // Formatar os dados para incluir porcentagens
-                const formattedData = Object.entries(response.data).map(([language, percentage]) => ({
-                    language,
-                    percentage: parseFloat(percentage), // manter como número para facilitar a renderização
-                }));
-
-                setChartData(formattedData);
-            } catch (error) {
-                console.log(error.message);
-            }
-        };
-
         fetchData();
     }, []);
 
+    function formatarData(data) {
+        const [ano, mes, dia] = data.split("-");
+        return `${dia}/${mes}/${ano}`;
+    }
+
     return (
-        <div className = 'github'> 
-            <h2>Distribuição de Linguagens no GitHub</h2>
-            {chartData.length>0 ? (
-              <ResponsiveContainer>
-              <BarChart data={chartData}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="language" />
-                  <YAxis />
-                  <Tooltip formatter={(value) => [`${value}%`, 'Porcentagem']} />
-                  <Legend />
-                  <Bar dataKey="percentage" fill="#312fb8" name="Porcentagem de Linguagens"/>
-              </BarChart>
-              </ResponsiveContainer>
-            ): <div>
-                    <LottieAnimationGithub/>
-                </div>}
-            
-        </div>
-    );
+        loading ? (
+            <LottieAnimationGithub />
+        ) : (
+
+            <div className='contribuicoes'>
+                {total && (<div className='total'>
+                    <h3>Contribuições do Github</h3>
+                    <p>Dados atualizados em tempo real</p>
+                </div>)}
+                {dados.length > 0 && (<div className='container'>
+                    {total && (<h4>{total} contribuições no último ano</h4>)}
+                    <div className='container-grafico'>
+                        <div className='meses'>
+                            {meses.length > 0 && (
+                                meses.map((mes, index) => (
+                                    <p key={index}>{mes}</p>
+                                ))
+                            )}
+                        </div>
+                        <div className='grafico'>
+                            <div className='semana'>
+                                <p>Segunda</p>
+                                <p>Quarta</p>
+                                <p>Sexta</p>
+                            </div>
+                            {dados.map((coluna, index) => (
+                                <div key={index} className={`coluna ${index === 0 && 'coluna1'}`}>
+                                    {coluna.contributionDays.map(dia => (
+                                        <div key={dia.date} className='dia'
+                                            style={{ backgroundColor: dia.color }}
+                                            title={dia.contributionCount === 0
+                                                ? 'Nenhuma contribuição em ' + formatarData(dia.date)
+                                                : dia.contributionCount === 1
+                                                    ? dia.contributionCount + ' contribuição em ' + formatarData(dia.date)
+                                                    : dia.contributionCount + ' contribuições em ' + formatarData(dia.date)}
+                                        >
+                                        </div>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                        <div className='legenda'>
+                        <p>Menos</p>
+                        <div className='dia' style={{backgroundColor:'#ebedf0'}}></div>
+                        <div className='dia' style={{backgroundColor:'#9be9a8'}}></div>
+                        <div className='dia' style={{backgroundColor:'#40c463'}}></div>
+                        <div className='dia' style={{backgroundColor:'#30a14e'}}></div>
+                        <div className='dia' style={{backgroundColor:'#216e39'}}></div>
+                        <p>Mais</p>
+                    </div>
+                    </div>
+                </div>)}
+            </div>
+        )
+    )
 }
--- pages/api/github.js
+++ pages/api/github.js
@@ -1,50 +1,46 @@
 import axios from "axios";
 
 export default async function github(req, res) {
+    const dataAtual = new Date();
+    const dataInicial = new Date(dataAtual); // Cria uma cópia de dataAtual
+    dataInicial.setFullYear(dataAtual.getFullYear() - 1);
+   
     try {
-        // Obter todos os repositórios do usuário
-        const reposResponse = await axios.get('https://api.github.com/users/KarinaBarros/repos', {
+        const query = `
+        {
+          user(login: "KarinaBarros") {
+            contributionsCollection(from: "${dataInicial.toISOString()}", to: "${dataAtual.toISOString()}") {
+              contributionCalendar {
+                totalContributions
+                weeks {
+                  contributionDays {
+                    date
+                    contributionCount
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      `;
+
+        const response = await axios.post('https://api.github.com/graphql',{ query }, {
             headers: {
-                Authorization: `token ${process.env.GITHUB}`,
-                Accept: 'application/vnd.github.v3+json', // Define a versão da API que você está usando
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.GITHUB}`,
             },
         });
+        let meses = [];
 
-        const repos = reposResponse.data;
-        const languagesData = {};
-
-        // Iterar sobre todos os repositórios para obter as linguagens
-        for (const repo of repos) {
-            const languageResponse = await axios.get(`https://api.github.com/repos/KarinaBarros/${repo.name}/languages`, {
-                headers: {
-                    Authorization: `token ${process.env.GITHUB}`,
-                    Accept: 'application/vnd.github.v3+json', // Define a versão da API que você está usando
-                },
-            });
-
-            const languages = languageResponse.data;
-
-            // Agregar os dados das linguagens
-            for (const [language, bytes] of Object.entries(languages)) {
-                if (languagesData[language]) {
-                    languagesData[language] += bytes;
-                } else {
-                    languagesData[language] = bytes;
-                }
-            }
+        while(dataInicial <= dataAtual){
+          let mes = dataInicial.toLocaleString('pt-BR', {month: 'short'});
+          mes =  mes.charAt(0).toUpperCase() + mes.slice(1).replace(/\.$/, "");
+          meses.push(mes);
+          dataInicial.setMonth(dataInicial.getMonth() + 1);
         }
-
-        // Calcular o total de bytes
-        const totalBytes = Object.values(languagesData).reduce((acc, value) => acc + value, 0);
-
-        // Calcular a porcentagem para cada linguagem
-        const percentages = {};
-        for (const [language, bytes] of Object.entries(languagesData)) {
-            percentages[language] = ((bytes / totalBytes) * 100).toFixed(2); // Arredondar para duas casas decimais
-        }
-
-        // Retorne os dados agregados em porcentagens
-        res.status(200).json(percentages);
+        const data = {meses:meses, github:response.data.data.user.contributionsCollection.contributionCalendar}
+        res.status(200).json(data);
     } catch (error) {
         console.error('Erro ao buscar dados do GitHub:', error);
         res.status(error.response?.status || 500).json({ error: 'Erro ao buscar dados do GitHub' });
