ebe3b89dd0cc44f605dd25f006879917bde214c6
Karina Barros <barros_kari@hotmail.com>
2026-02-25 12:20:38 -0300

    Melhora de performance na  home

--- components/home/header/Header.js
+++ components/home/header/Header.js
@@ -3,33 +3,19 @@ import './header.css';
 import HeaderText from './header_text';
 
 const Header = ({ tema }) => {
-    const [dev, setDev] = useState('');
-
-    useEffect(() => {
-        if (tema === 'dark') {
-            setDev('/dev.png');
-        }
-        if (tema === 'light') {
-            setDev('/dev2.png')
-        }
-        if (!tema) {
-            setDev('/dev.png');
-        }
-    }, [tema])
 
     return (
         <header>
             <div className='header'>
                 <HeaderText />
-                <div className='container-center'>
+                {(tema && <div className='container-center'>
                     <img className='avatar' src='/avatar.png' alt='Desenvolvedor' />
-                    {dev && (
-                        <video className='video' controls autoPlay loop muted>
-                            <source src='/matrix_-_5442 (540p).mp4' type="video/mp4" />
-                        </video>
-                    )}
-                    <img src={dev} alt='dev' className='dev'></img>
-                </div>
+                    <video className='video' controls autoPlay loop muted>
+                        <source src='/matrix_-_5442 (540p).mp4' type="video/mp4" />
+                    </video>
+                    <img src="/dev.png" className={`dev ${tema === 'dark' ? 'show' : 'hide'}`} />
+                    <img src="/dev2.png" className={`dev ${tema === 'light' ? 'show' : 'hide'}`} />
+                </div>)}
             </div>
         </header>
     );
