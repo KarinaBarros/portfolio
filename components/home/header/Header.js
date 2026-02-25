751db6516ce2ae1e193c33b37936dc6f80c9a206
Karina Barros <barros_kari@hotmail.com>
2026-02-25 13:54:00 -0300

    melhora performance header home

--- components/home/header/Header.js
+++ components/home/header/Header.js
@@ -3,19 +3,32 @@ import './header.css';
 import HeaderText from './header_text';
 
 const Header = ({ tema }) => {
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setReady(true);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <header>
             <div className='header'>
-                <HeaderText />
-                {(tema && <div className='container-center'>
-                    <img className='avatar' src='/avatar.png' alt='Desenvolvedor' />
-                    <video className='video' controls autoPlay loop muted>
-                        <source src='/matrix_-_5442 (540p).mp4' type="video/mp4" />
-                    </video>
-                    <img src="/dev.png" className={`dev ${tema === 'dark' ? 'show' : 'hide'}`} />
-                    <img src="/dev2.png" className={`dev ${tema === 'light' ? 'show' : 'hide'}`} />
-                </div>)}
+                {(ready &&
+                    <>
+                        <HeaderText />
+                        {(tema && <div className='container-center'>
+                            <img className='avatar' src='/avatar.png' alt='Desenvolvedor' />
+                            <video className='video' controls autoPlay loop muted>
+                                <source src='/matrix_-_5442 (540p).mp4' type="video/mp4" />
+                            </video>
+                            <img src="/dev.png" className={`dev ${tema === 'dark' ? 'show' : 'hide'}`} />
+                            <img src="/dev2.png" className={`dev ${tema === 'light' ? 'show' : 'hide'}`} />
+                        </div>)}
+                    </>
+                )}
             </div>
         </header>
     );
